8cc10e51c2347d3e65a8dba10f0b88cedc4e0358
Claudio Braga Leite <claudiobl2010@hotmail.com>
2012-02-05 21:42:52 -0200

    heranca de template e colocando as coisas para funcionar so com html puro

--- cagada/models.py
+++ cagada/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Url(models.Model):
+    titulo = models.CharField(max_length=200, null=False)
+    descricao = models.CharField(max_length=500, null=False)
+    url = models.CharField(max_length=1000, null=False)
+    qtd = models.IntegerField(null=False, default=1)
+    ranking = models.IntegerField(null=False, default=0)
+    hash = models.CharField(max_length=100, null=False)
+    criacao = models.DateTimeField(null=False, auto_now_add=True)
+
+class Assunto(models.Model):
+    descricao = models.CharField(max_length=1000, null=False)
+    qtd = models.IntegerField(null=False)
+    ranking = models.IntegerField(null=False, default=0)
+    hash = models.CharField(max_length=100, null=False)
+    criacao = models.DateTimeField(null=False, auto_now_add=True)
+
+class Log(models.Model):
+    cagada_id = models.IntegerField(null=False)
+    tipo = models.CharField(max_length=1, null=False, choices=(('U', 'Url'), ('A', 'Assunto')))
+    ip = models.CharField(max_length=15, null=False)
+    data = models.DateTimeField(null=False, auto_now_add=True)
