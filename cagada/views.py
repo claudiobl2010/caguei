1b25bb4f9fc4868b1271c0d5d2506285f59de8ac
= <cyberelfo@gmail.com>
2012-02-24 20:11:38 -0200

    Continuando o post da URL e salvando no banco

--- cagada/views.py
+++ cagada/views.py
@@ -12,6 +12,7 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
 from cagada.models import Url, Assunto, Log
+from django.db.models import F
 
 def home(request):
 
@@ -30,29 +31,37 @@ def cagar(request):
     # Verifica se foi atingida a QTD máxima de cagadas por hora (máximo de 100 cagadas)
     data_ultima_hora = datetime.now() - timedelta(hours=1)
     qtd = Log.objects.filter(ip=request.META['REMOTE_ADDR'], data__gt=data_ultima_hora).count()
-    if qtd > 3:
+    if qtd > 100:
         return HttpResponse(simplejson.dumps({'tipo':'ERROR', 'msg':'Usuário atingiu a quantidade máxima de cagadas por hora!!!'}), content_type="application/json; charset=UTF-8")
-    
-    
-    #################################
-    
-    try:
-        page = urllib2.urlopen(url, timeout=3)
-    except Exception, e:
-        log = Log(cagada_id=0, tipo='E', ip=request.META['REMOTE_ADDR'])
-        log.save()
-        return HttpResponse(simplejson.dumps({'tipo':'ERROR', 'msg':'URL informada não pode ser carregada!!!'}), content_type="application/json; charset=UTF-8")
+        
+    urls = Url.objects.filter(hash = hash)
 
-    content = page.read()
-    
-    document = BeautifulSoup(content)
-    
-    titulo = document.head.title.string
-    descricao = document.find('meta', {'name':'description'}).get('content')
+    if urls:
+        Url.objects.update(qtd = F('qtd') + 1)
+    else:
+        try:
+            page = urllib2.urlopen(url, timeout=3)
+        except Exception, e:
+            log = Log(cagada_id=0, tipo='E', ip=request.META['REMOTE_ADDR'])
+            log.save()
+            return HttpResponse(simplejson.dumps({'tipo':'ERROR', 'msg':'URL informada não pode ser carregada!!!'}), content_type="application/json; charset=UTF-8")
 
-    #################################
+        content = page.read()
+        document = BeautifulSoup(content)
 
-    
+        try:
+            titulo = document.head.title.string
+        except Exception, e:
+            titulo = "Não encontrado"
+
+        try:
+            descricao = document.find('meta', {'name':'description'}).get('content')
+        except Exception, e:
+            descricao = "Não encontrado"
+
+        url_obj = Url(titulo = titulo, descricao = descricao, url = url, qtd = 1, hash = hash)
+        url_obj.save()
+
     # Joga o Hash da URL cagada na Session
     cagadas.append(hash)
     request.session['cagadas'] = ','.join(cagadas)
@@ -60,5 +69,9 @@ def cagar(request):
     # Log do IP client que deu a cagada na URL
     log = Log(cagada_id=1, tipo='U', ip=request.META['REMOTE_ADDR'])
     log.save()
+
+    ################################
+    # falta tratar o json de sucesso
+    # falta tratar as msgs
 
     return HttpResponse(simplejson.dumps({'tipo': 'SUCCESS', 'msg': 'Ok!!!'}), content_type="application/json; charset=UTF-8")
