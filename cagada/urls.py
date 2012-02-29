8023b9872ce4425629515de62dc6727f6ad7affe
= <cyberelfo@gmail.com>
2012-02-29 20:23:00 -0300

    Criar assunto

--- cagada/urls.py
+++ cagada/urls.py
@@ -5,5 +5,7 @@ urlpatterns = patterns('caguei.cagada.views',
     (r'^$', 'home'),
     (r'^cagar$', 'cagar'),
     (r'^url/(?P<url_id>\d+)/?$', 'url_detalhe'),
+    (r'^criar-assunto/?$', 'criar_assunto'),
+    (r'^cagar-assunto/?$', 'cagar_assunto'),
 
 )
--- cagada/views.py
+++ cagada/views.py
@@ -73,7 +73,7 @@ def cagar(request):
     request.session['cagadas'] = ','.join(cagadas)
     
     # Log do IP client que deu a cagada na URL
-    log = Log(cagada_id=1, tipo='U', ip=request.META['REMOTE_ADDR'])
+    log = Log(cagada_id=url_obj.id, tipo='U', ip=request.META['REMOTE_ADDR'])
     log.save()
     
     return HttpResponse(simplejson.dumps({'tipo':'SUCCESS', 'msg':'Você e + %s pessoas cagaram para isso.' % url_obj.qtd if urls else 'Você acaba de dar uma nova cagada. Parabéns!', 'url': {'id': url_obj.id, 'qtd': url_obj.qtd}}), content_type="application/json; charset=UTF-8")
@@ -86,3 +86,45 @@ def url_detalhe(request, url_id):
     url_obj.caguei = True if cagadas.count(url_obj.hash) else False
 
     return render_to_response('url-detalhe.html', {'url':url_obj})
+
+def criar_assunto(request):
+
+    return render_to_response('criar-assunto.html')
+
+def cagar_assunto(request):
+    # Hash do assunto
+    assunto = request.POST['assunto']
+    hash = hashlib.md5(assunto.lower()).hexdigest()
+
+    # Verifica se usuário já cagou para o assunto
+    cagadas = request.session.get('cagadas').split(',') if request.session.get('cagadas') else []
+    if cagadas.count(hash):
+        return HttpResponse(simplejson.dumps({'tipo':'ERROR', 'msg':'Você já cagou para esse assunto.'}), content_type="application/json; charset=UTF-8")
+
+    # Verifica se foi atingida a QTD máxima de cagadas por hora (máximo de 100 cagadas)
+    data_ultima_hora = datetime.now() - timedelta(hours=1)
+    qtd = Log.objects.filter(ip=request.META['REMOTE_ADDR'], data__gt=data_ultima_hora).count()
+    if qtd > 100:
+        return HttpResponse(simplejson.dumps({'tipo':'ERROR', 'msg':'Você atingiu a quantidade máxima de cagadas por hora.'}), content_type="application/json; charset=UTF-8")
+
+    # Verifica se o assunto já foi cagada alguma vez
+    assuntos = Assunto.objects.filter(hash=hash)
+
+    if assuntos:
+        # Se o assunto já foi cagado alguma vez, basta somar + 1 a sua quantidade
+        Assunto.objects.update(qtd=F('qtd')+1)
+        assunto_obj = assuntos[0]
+    else:
+
+        assunto_obj = Assunto(descricao=assunto, hash=hash, qtd=1)
+        assunto_obj.save()
+
+    # Joga o Hash do assunto cagado na Session
+    cagadas.append(hash)
+    request.session['cagadas'] = ','.join(cagadas)
+
+    # Log do IP client que deu a cagada na URL
+    log = Log(cagada_id=assunto_obj.id, tipo='A', ip=request.META['REMOTE_ADDR'])
+    log.save()
+
+    return HttpResponse(simplejson.dumps({'tipo':'SUCCESS', 'msg':'Você e + %s pessoas cagaram para isso.' % assunto_obj.qtd if assuntos else 'Você acaba de dar uma nova cagada. Parabéns!', 'assunto': {'id': assunto_obj.id, 'qtd': assunto_obj.qtd}}), content_type="application/json; charset=UTF-8")
